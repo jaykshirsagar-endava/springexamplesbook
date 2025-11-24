5b872087208209c0964d2a05add8ac64614deadb
jkshirsagar <jay.kshirsagar@endava.com>
2025-11-24 11:45:59 +0200

    finish chapter 1

--- src/main/java/com/jetbrains/marco/App.java
+++ src/main/java/com/jetbrains/marco/App.java
@@ -4,6 +4,8 @@ import com.jetbrains.marco.config.ProjectConfig;
 import com.jetbrains.marco.model.Parrot;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.function.Supplier;
+
 /**
  * Hello world!
  *
@@ -15,6 +17,16 @@ public class App
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
         Parrot p = context.getBean("parrot2", Parrot.class);
         System.out.println(p.getName());
+
+        Parrot x = new Parrot();
+        x.setName("Koko");
+        Supplier<Parrot> parrotSupplier = () -> x;
+
+        context.registerBean("parrot4",
+                Parrot.class, parrotSupplier);
+
+        Parrot p4 = context.getBean("parrot4",Parrot.class);
+        System.out.println(p4.getName());
 //        String s = context.getBean(String.class);
 //        System.out.println(s);
 //        Integer n = context.getBean(Integer.class);
--- src/main/java/com/jetbrains/marco/model/Parrot.java
+++ src/main/java/com/jetbrains/marco/model/Parrot.java
@@ -3,8 +3,14 @@ package com.jetbrains.marco.model;
 import lombok.Data;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+
 @Data
 @Component
 public class Parrot {
    private String name;
+//    @PostConstruct
+//    public void init() {
+//        this.name = "Kiki";
+//    }
 }
