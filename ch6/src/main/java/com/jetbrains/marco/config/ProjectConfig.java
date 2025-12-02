d287e68e110184aa50d947bbfa8215ffbdd8ddb8
jkshirsagar <jay.kshirsagar@endava.com>
2025-12-02 14:38:30 +0200

    chapter 6 step by step

--- ch6/src/main/java/com/jetbrains/marco/config/ProjectConfig.java
+++ ch6/src/main/java/com/jetbrains/marco/config/ProjectConfig.java
@@ -1,9 +1,17 @@
 package com.jetbrains.marco.config;
 
+import com.jetbrains.marco.model.LoggingAspect;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
 @ComponentScan(basePackages = "com.jetbrains.marco.service")
+@EnableAspectJAutoProxy
 public class ProjectConfig {
+    @Bean
+    public LoggingAspect aspect() {
+        return new LoggingAspect();
+    }
 }
--- ch6/src/main/java/com/jetbrains/marco/service/CommentService.java
+++ ch6/src/main/java/com/jetbrains/marco/service/CommentService.java
@@ -1,13 +1,14 @@
 package com.jetbrains.marco.service;
 
 import com.jetbrains.marco.model.Comment;
+import com.jetbrains.marco.model.LoggingAspect;
 import org.springframework.stereotype.Service;
 
 import java.util.logging.Logger;
 
 @Service
 public class CommentService {
-    private Logger logger = Logger.getLogger(CommentService.class.getName());
+    private Logger logger = Logger.getLogger(LoggingAspect.class.getName());
 
     public void publishComment(Comment comment) {
         logger.info("Publishing comment:" + comment.getText());
