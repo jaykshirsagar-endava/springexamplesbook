34528271d39fba8dccc2af99c584b7842ecbd9da
jkshirsagar <jay.kshirsagar@endava.com>
2025-12-05 15:04:06 +0200

    chapter 10

---
+++ ch10/src/test/java/com/example/MainTests.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import com.example.exceptions.NotEnoughMoneyException;
+import com.example.model.ErrorDetails;
+import com.example.model.PaymentDetails;
+import com.example.service.PaymentService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class MainTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
