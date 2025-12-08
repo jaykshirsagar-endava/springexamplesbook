392cf4239b2c1e17ee57896d4a00ddc3383b3155
jkshirsagar <jay.kshirsagar@endava.com>
2025-12-08 10:40:23 +0200

    chapter 11 RestTemplate

--- ch11/src/main/java/com/example/controllers/PaymentsController.java
+++ ch11/src/main/java/com/example/controllers/PaymentsController.java
@@ -11,17 +11,16 @@ import java.util.UUID;
 @RestController
 public class PaymentsController {
 
-  private final PaymentsProxy paymentsProxy;
+    private final PaymentsProxy paymentsProxy;
 
-  public PaymentsController(PaymentsProxy paymentsProxy) {
-    this.paymentsProxy = paymentsProxy;
-  }
+    public PaymentsController(PaymentsProxy paymentsProxy) {
+        this.paymentsProxy = paymentsProxy;
+    }
 
-  @PostMapping("/payment")
-  public Payment createPayment(
-      @RequestBody Payment payment
-      ) {
-    String requestId = UUID.randomUUID().toString();
-    return paymentsProxy.createPayment(requestId, payment);
-  }
+    @PostMapping("/payment")
+    public Payment createPayment(
+            @RequestBody Payment payment
+    ) {
+        return paymentsProxy.createPayment(payment);
+    }
 }
